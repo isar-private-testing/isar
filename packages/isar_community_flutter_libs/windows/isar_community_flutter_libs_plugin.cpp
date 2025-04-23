88dd342e4072da89ee97e40b26ddf779cba83e22
Vicente Russo <vicente.russo@gmail.com>
2025-04-22 23:23:24 -0300

    renamed to win

--- packages/isar_community_flutter_libs/windows/isar_community_flutter_libs_plugin.cpp
+++ packages/isar_community_flutter_libs/windows/isar_community_flutter_libs_plugin.cpp
@@ -1,4 +1,4 @@
-#include "include/isar_flutter_libs/isar_flutter_libs_plugin.h"
+#include "include/isar_community_flutter_libs/isar_flutter_libs_plugin.h"
 
 // This must be included before many other Windows headers.
 #include <windows.h>
